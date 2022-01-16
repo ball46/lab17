50b7b2daa2d1a932bfc16c644507fb3ce367945f
charnchol <khumball46@gmail.com>
2022-01-16 13:57:18 +0700

    lab 17-1 complete

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,43 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *a, int N, int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << a[i];
+        if(i %M == (M-1)) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *b,int N,int M){
+	for(int i = 0; i < N*M; i++){
+    	b[i] = (rand()%101);
+		b[i] /= 100;
+    }
+}
+
+void findRowSum(const double *c,double *d,int N,int M){
+	for(int j = 0; j < N; j++){
+		double l = 0;
+    	for(int k = 0; k < M; k++){
+			l += *((c+k)+(M*j));
+		}
+		d[j] = l ;
+    }
+}
+
+void findColSum(const double *e,double *f,int N,int M){
+	int i = 0, j = 0;
+    f[j] = 0;
+    for(int r = 0; r < M; r++){
+        i = r;
+        for(int k = 0; k < N; k++){
+            f[j] += e[i];
+            i += M;
+        }
+        j++;
+        f[j] = 0;
+    }
+}
